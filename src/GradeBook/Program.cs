d9af3489848a6cdccf61dd8421e547ce7573c188
Pär Nygren <par.a.nygren@gmail.com>
2019-04-25 19:16:39 +0200

    Use application input arguments

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,7 +6,7 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Pär!");
+            Console.WriteLine($"Hello, {args[0]}!");
         }
     }
 }
