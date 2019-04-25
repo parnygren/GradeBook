ac34eea2451890bede69f4532a1e7ca5af986cee
Pär Nygren <par.a.nygren@gmail.com>
2019-04-25 20:10:00 +0200

    Using a list and compute the average

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,13 +8,15 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
 
             var result = 0.0;
-            foreach (var number in numbers)
+            foreach (var number in grades)
             {
                 result += number;
             }
-            Console.WriteLine(result);
+            Console.WriteLine(result/grades.Count);
 
             if (args.Length > 0)
             {
