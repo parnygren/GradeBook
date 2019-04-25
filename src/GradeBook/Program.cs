af4ebffe010d45666d8a032a4231fd7be65584ca
Pär Nygren <par.a.nygren@gmail.com>
2019-04-25 20:01:30 +0200

    Adding numbers

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,7 +6,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            if(args.Length > 0)
+            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+
+            var result = 0.0;
+            foreach (var number in numbers)
+            {
+                result += number;
+            }
+            Console.WriteLine(result);
+
+            if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
             }
