625486cca0602b723211196ad72ae81e0967c266
Pär Nygren <par.a.nygren@gmail.com>
2019-04-27 08:18:55 +0200

    Refactoring

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -16,22 +16,22 @@ namespace GradeBook
             grades.Add(grade);
         }
 
-        public void ShowStatistics()
+        public Statistics GetStatistics()
         {
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.Low = double.MinValue;
+            result.High = double.MaxValue;
 
-            foreach (var number in grades)
+            foreach (var grade in grades)
             {
-                lowGrade = Math.Min(number, lowGrade);
-                highGrade = Math.Max(number, highGrade);
-                result += number;
+                result.Low = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;
             }
-            result /= grades.Count;
-            Console.WriteLine($"The lowest grade is {lowGrade}");
-            Console.WriteLine($"The highest grade is {highGrade}");
-            Console.WriteLine($"The average grade is {result:N1}");
+            result.Average /= grades.Count;
+
+            return result;
         }
 
         List<double> grades;
