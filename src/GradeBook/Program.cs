7540d3de30aebfb635f0e8eea61b8a8b2f799a36
Pär Nygren <par.a.nygren@gmail.com>
2019-04-30 10:04:33 +0200

    Take user input from console

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,11 +8,19 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Pär's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            var input = "";
+            while(true) {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                input = Console.ReadLine();
+                if(input == "q") {
+                    break;
+                }
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+            };
 
             var stats = book.GetStatistics();
+            
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
