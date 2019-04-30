ca180bb9be4ca4ec17d0d19b65cfa51e6562adfc
Pär Nygren <par.a.nygren@gmail.com>
2019-04-30 10:28:07 +0200

    Catch exceptions

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,18 +9,36 @@ namespace GradeBook
         {
             var book = new Book("Pär's Grade Book");
             var input = "";
-            while(true) {
+            while (true)
+            {
                 Console.WriteLine("Enter a grade or 'q' to quit");
                 input = Console.ReadLine();
-                if(input == "q") {
+                if (input == "q")
+                {
                     break;
                 }
-                var grade = double.Parse(input);
-                book.AddGrade(grade);
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
             };
 
             var stats = book.GetStatistics();
-            
+
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
